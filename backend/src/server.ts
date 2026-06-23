7d6b9fa630b8d8517182210aabb3e06213aea87e
helder0919 <heldervini2004@gmail.com>
2026-06-23 15:34:43 -0300

    Create server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando com sucesso na porta ${PORT}!`);
+});
